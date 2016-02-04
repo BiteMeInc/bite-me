7b24420e3667bdfe7830130ccc0fa8f3df09ad0d
Jeremy Smellie <jeremysmellie@gmail.com>
2016-02-03 21:37:22 -0500

    Summary:   * Put in placeholder tiles in the prototype scene so you can tell that the player is moving.

--- Assets/Prototypes/PlayerMovement/Scripts/TileMaker.cs
+++ Assets/Prototypes/PlayerMovement/Scripts/TileMaker.cs
@@ -57,6 +57,63 @@ using System.Collections;
 		#endregion
 	
 		#region Private Methods
+        [ContextMenu("Populate Grid")]
+        private void PopulateGrid()
+        {
+            ClearGrid();
+
+            if (m_OriginalTile != null)
+            {
+                m_TileGrid = new BoxCollider2D[(int)m_TileGridSize.x, (int)m_TileGridSize.y];
+
+                Vector2 tileSize = m_OriginalTile.size;
+                Vector2 gridDimesions = new Vector2(tileSize.x * m_TileGridSize.x, tileSize.y * m_TileGridSize.y);
+                Vector2 halfTileSize = tileSize * 0.5f;
+
+                BoxCollider2D curTile;
+                for(int x = 0; x < m_TileGrid.GetLength(0); ++x)
+                {
+                    for (int y = 0; y < m_TileGrid.GetLength(1); ++y)
+                    {
+                        curTile = m_TileGrid[x, y] = GameObject.Instantiate<BoxCollider2D>(m_OriginalTile);
+                        Vector3 tilePos = Vector3.zero;
+                        tilePos.x = (-gridDimesions.x / 2) + (tileSize.x * x) + halfTileSize.x;
+                        tilePos.y = (-gridDimesions.y / 2) + (tileSize.x * y) + halfTileSize.y;
+                        tilePos.z = m_OriginalTile.transform.position.z;
+                        curTile.transform.position = tilePos;
+                        curTile.transform.parent = this.transform;
+                        curTile.name = string.Format("Tile[{0},{1}]", x, y);
+                    }
+                }
+            }
+
+
+        }
+
+        private void ClearGrid()
+        {
+            if (m_TileGrid != null && m_TileGrid.GetLength(0) > 0)
+            {
+                BoxCollider2D curTile = null;
+
+                for (int x = 0; x < m_TileGrid.GetLength(0); ++x)
+                {
+                    for (int y = 0; y < m_TileGrid.GetLength(1); ++y)
+                    {
+                        curTile = m_TileGrid[x, y];
+
+                        if (curTile != null)
+                        {
+#if UNITY_EDITOR
+                            DestroyImmediate(curTile.gameObject);
+#else
+                            Destroy(curTile.gameObject);
+#endif
+                        }
+                    }
+                }
+            }
+        }
 		#endregion
 	}
 	
